b4b4187a714fb83ba187a1c7ce53dd6e32b14c0d
Mohamed Ayman <madoo.ayman14@gmail.com>
2019-03-04 20:26:06 +0200

    Add FlowNet2 code

---
+++ flownet2-pytorch/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e 
+
+cd ./networks/correlation_package
+python setup.py install --user
+cd ../resample2d_package 
+python setup.py install --user
+cd ../channelnorm_package 
+python setup.py install --user
+cd ..
