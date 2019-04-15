50f6510f1ca61512364efdd56cc5a9f5adabfffb
Mohamed Ayman <madoo.ayman14@gmail.com>
2019-04-15 20:50:04 +0200

    Fix the dimensions of the flow and low batches

--- preprocess_data.py
+++ preprocess_data.py
@@ -8,8 +8,7 @@ fileConfig('logging_config.ini')
 
 
 src_video = '/home/hamada/dataset/backup/Bella Italia pt2 4K-174952003.mp4'
-dest_dataset = 'd1.hdf5'
 
 flow_net = FlowModelWrapper.getInstance()
 
-tranform_video(src_video, dest_dataset, flow_net)
+tranform_video(src_video, flow_net)
