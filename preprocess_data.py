20eca3eea0d73abb599fc41e530f97d5b28d4c58
Moamen <moamenelbaroudy.me@gmail.com>
2019-04-16 17:04:21 +0200

    Implementing a part of the model

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,7 +1,7 @@
 from flow_model_wrapper import FlowModelWrapper
 from frvsr.pre_processing.video_transformation import tranform_video
 
-src_video = '/home/hamada/dataset/backup/Bella Italia pt2 4K-174952003.mp4'
+src_video = '/home/moamen/gp/Video-Enhancement/dataset/4K Cinema Camera - With Voiceover-121649159.mp4'
 dest_dataset = 'd1.hdf5'
 
 flow_net = FlowModelWrapper.getInstance()
